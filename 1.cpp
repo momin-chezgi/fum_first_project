ad2a1bf30f9c7d902176c93c312b789ec19c825b
momin.chezgi <momin.chezgi@gmail.com>
2025-12-01 08:08:50 +0330

    The first task(10%) has been completed

--- 1.cpp
+++ 1.cpp
@@ -1,7 +1,68 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <cstdlib>
+#include <cmath>
+
+int n,m, drnum, mnnum, wlnum;
+int source[2];
 using namespace std;
 
+void printGrid(char *grid);
+
 int main(void){
-    cout << "Hello, World!" << endl;
-    return 0;          
+    cin >> n >> m;
+    char grid[2 * n+1][2 * m+1];
+    for (int i = 0; i <= n; i++)
+    {
+        for(int j=0; j<= m; j++){
+            grid[2*i+1][2*j+1] = '.';
+            grid[2 * i][2 * j] = '#';
+            grid[2 * i][2 * j + 1] = ' ';
+            grid[2 * i + 1][2 * j] = ' ';
+        }
+    }
+    //printGrid(&grid[0][0]);
+    cin >> source[0] >> source[1];
+    grid[2*source[0]+1][2*source[1]+1] = 'S';
+    //printGrid(&grid[0][0]);
+    cin >> drnum;
+    for (int i = 0; i<drnum; i++){
+        int x, y;
+        cin >> x >> y;
+        grid[2*x+1][2*y+1] = 'D';
+    }
+    //printGrid(&grid[0][0]);
+    cin >> mnnum;
+    for (int i = 0; i<mnnum; i++){
+        int x, y;
+        cin >> x >> y;
+        grid[2*x+1][2*y+1] = 'M';
+    }
+    //printGrid(&grid[0][0]);
+    cin >> wlnum;
+    for (int i = 0; i < wlnum; i++){
+        int x, y;
+        char wltype;
+        cin >> x >> y >> wltype;
+        if(wltype == 'H'){
+            grid[2*x+2][2*y + 1] = '#';  
+        }
+        else{
+            grid[2*x+1][2*y+2] = '#';
+        }
+    }
+    printGrid(&grid[0][0]);
+    return 0;
+}
+
+void printGrid(char *grid){
+    system("clear");
+    for (int i = 0; i <= 2*n; i++)
+    {
+        for(int j=0; j <= 2*m; j++){
+            cout << *(grid + (i * (2*m+1) + j));
+        }
+        cout << endl;
+    }
 }
