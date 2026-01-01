e207eb7b92e9b85c10158aa1ed4348fbe1e70a8d
Mohammad Amin Chezgi <momin.chezgi@gmail.com>
2026-01-01 09:40:40 +0330

    Just a tiny change!

--- 4.cpp
+++ 4.cpp
@@ -186,10 +186,10 @@ intpair Im_hungry(vec2d(char)& grid, intpair mnpos, vec2d(int)& has_seen){
             int y = neigh.second;
             if(has_seen[x][y]==1) continue;
             has_seen[x][y] = 1;
-            if(grid[x+1][y]!='#') breadth2.push_back({x+2,y});
-            if(grid[x][y+1]!='#') breadth2.push_back({x,y+2});
-            if(grid[x-1][y]!='#') breadth2.push_back({x-2,y});
-            if(grid[x][y-1]!='#') breadth2.push_back({x,y-2});
+            if(grid[x+1][y]==' ') breadth2.push_back({x+2,y});
+            if(grid[x][y+1]==' ') breadth2.push_back({x,y+2});
+            if(grid[x-1][y]==' ') breadth2.push_back({x-2,y});
+            if(grid[x][y-1]==' ') breadth2.push_back({x,y-2});
         }
         breadth1 = breadth2;
         breadth2.clear();
